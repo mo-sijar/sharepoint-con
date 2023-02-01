f79a1c77ac4abad54c30e5fcf8a05b113e5d597e
Mo <mo.sijar@gmail.com>
2023-02-01 14:29:39 +0100

    used setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(name='sharepointing',
-      version='0.1.3',
+setuptools.setup(name='sharepointing',
+      version='0.1.4',
       description='A package to establish a connection to SharePoint site, and upload files through that connection',
       author='Mo Sijarrey',
       author_email='mo.sijar@gmail.com',
