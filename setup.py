0e07a78fa89eee0d6fda0cfd82ae7773c743390a
Mo <mo.sijar@gmail.com>
2023-02-01 14:38:15 +0100

    reverted to static install_requirements

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@
 
 import setuptools
 
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
-
 setuptools.setup(name='sharepointing',
       version='0.1.5',
       description='A package to establish a connection to SharePoint site, and upload files through that connection',
       author='Mo Sijarrey',
       author_email='mo.sijar@gmail.com',
       url='https://github.com/mo-sijar/sharepointing',
-      install_requires=REQUIREMENTS
+      install_requires=['Office365-REST-Python-Client>=2.3.16']
      )
