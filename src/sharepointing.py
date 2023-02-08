2170c7ca0ccda357a961414acdd582e774123d55
Mo <mo.sijar.inso@gmail.com>
2023-02-08 10:32:35 +0100

    added download_file function, and changed send_file to upload_file

--- src/sharepointing.py
+++ src/sharepointing.py
@@ -39,23 +39,23 @@ class sp_site:
         except:
             raise Exception("[Error] Could not read the file contents, make sure it's available with correct path.")
 
-    def send_file(self,file,target_url) -> bool:
+    def upload_file(self,local_file,remote_target_folder) -> bool:
         """
-        Function to send a @file within the local premise to a sharepoint @target_folder
+        Function to upload a @local_file within the local storage to a sharepoint @remote_target_url
         It shouldn't have the library url, so no http link and it has only / (backslashes) 
         This will return True if the file was uploaded successfully
-        A good example is: "/teams/TechnologyInnovation/Shared Documents/General/Information Management/
+        A good example is: "/teams/MarketingTeam/Shared Documents/General/Marketing Reports/"
         """
 
         # Read the file content
-        file_content = self.read_file(file)
+        file_content = self.read_file(local_file)
 
         # Extract the file's name
-        file_name = ntpath.basename(file)
+        file_name = ntpath.basename(local_file)
 
         try:
             # Get actual folder from server
-            folder = self.ctx.web.get_folder_by_server_relative_url(target_url)
+            folder = self.ctx.web.get_folder_by_server_relative_url(remote_target_folder)
         except:
             raise Exception("[Error] Could not get to the folder, make sure the path is correct")
         
@@ -65,7 +65,31 @@ class sp_site:
 
             return True
         except:
-            raise Exception("[Error] Could not upload the file, make sure it's not read-only and you have proper")
+            raise Exception("[Error] Could not upload the file, make sure it's not read-only and you have the proper access.")
+
+    def download_file(self,remote_file,local_target_folder) -> bool:
+        """
+        Function to download a @remote_file from a SharePoint site to the local folder @local_target_folder
+        #Example#
+        remote_file = "/teams/MarketingTeam/Shared Documents/General/Marketing Reports/data.csv"
+        local_target_folder = "/home/username" # you can pass empty string to get the file where the code is running
+
+        This will return True if the file was uploaded successfully
+        """
+
+        try:
+            file_name = os.path.basename(remote_file)
+            local_target_file = os.path.join(local_target_folder,file_name)
+        except:
+            raise Exception("[Error] Could not parse remote_file or local_target_folder, make sure the format is correct.")
+        
+        try:
+            with open(local_target_file,'wb') as file:
+                file = self.ctx.web.get_file_by_server_relative_url(remote_file).download(file).execute_query()
+
+            return True
+        except:
+            raise Exception("[Error] Could not download the file, make sure the file exists and you have access to download it.")
 
 def decode_link(full_link,option) -> str:
     """
