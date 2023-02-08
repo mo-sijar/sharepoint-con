0b24a44ab4961cc87c6e736d2e0ce49e73a741de
Mo <mo.sijar.inso@gmail.com>
2023-02-08 10:36:54 +0100

    updated version number to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 
 setuptools.setup(name='sharepointing',
-      version='0.1.6',
+      version='0.2.0',
       description='A package to establish a connection to SharePoint site, and upload files through that connection',
       author='Mo Sijarrey',
       author_email='mo.sijar@gmail.com',
